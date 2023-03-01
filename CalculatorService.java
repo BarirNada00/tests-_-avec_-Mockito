b142052868ebd84099c46967dcf0e46822ec570d
BarirNada00 <116973510+BarirNada00@users.noreply.github.com>
2023-03-01 17:49:52 +0100

    Add files via upload

---
+++ CalculatorService.java
@@ -0,0 +1,7 @@
+package ma.projet.android.calculapplication;
+public interface CalculatorService {
+    public double add(double input1, double input2);
+    public double sous(double input1, double input2);
+    public double multi(double input1, double input2);
+    public double div(double input1, double input2);
+}
